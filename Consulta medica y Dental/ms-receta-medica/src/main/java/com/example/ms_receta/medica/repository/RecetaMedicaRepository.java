e1fcdf6f9ebc68591e94e11fd36faba30a9391be
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-06-19 05:23:43 -0400

    feat: update :(

--- Consulta medica y Dental/ms-receta-medica/src/main/java/com/example/ms_receta/medica/repository/RecetaMedicaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ms_receta.medica.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ms_receta.medica.model.RecetaMedica;
-
-public interface RecetaMedicaRepository extends JpaRepository<RecetaMedica, Long>{
-
-}
